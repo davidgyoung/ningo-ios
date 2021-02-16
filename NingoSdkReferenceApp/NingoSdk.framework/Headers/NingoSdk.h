65b912e4c31a2004f9ac1a197941485166e86bde
David G. Young <davidgyoung@gmail.com>
2021-02-16 13:04:49 -0500

    commit to beconscope repo

---
+++ NingoSdkReferenceApp/NingoSdk.framework/Headers/NingoSdk.h
@@ -0,0 +1,18 @@
+//
+//  NingoSdk.h
+//  NingoSdk
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NingoSdk.
+FOUNDATION_EXPORT double NingoSdkVersionNumber;
+
+//! Project version string for NingoSdk.
+FOUNDATION_EXPORT const unsigned char NingoSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NingoSdk/PublicHeader.h>
+
+
+
